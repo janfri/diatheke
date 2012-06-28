30489dd0950c3286536ee5d35311587c95894eeb
Jan Friedrich <janfri26@gmail.com>
2012-06-28 18:54:40 +0200

    New method Diatheke.search.

--- lib/diatheke.rb
+++ lib/diatheke.rb
@@ -1,3 +1,5 @@
+require 'shellwords'
+
 module Diatheke
 
   class << self
@@ -6,15 +8,37 @@ module Diatheke
       call('system', 'modulelistnames').split(/\n/)
     end
 
-    def passage(book, key)
-      call(book, key)
+    def passage(mod, key)
+      call(mod, key)
+    end
+
+    def search(mod, key, opts={})
+      search_type = :phrase
+      search_key = key
+      case key
+      when Regexp
+        search_type = :regex
+        search_key = key.inspect.sub(%r(^/)).sub(%r(/$))
+      when Array
+        search_type = :multiword
+        search_key = key.join(' ')
+      end
+      opts[:search_type] = search_type
+      call(mod, search_key, opts)
     end
 
     private
 
-    def call book, key, opts={}
-      opts_str = ''
-      cmd = "diatheke -b #{book} #{opts_str} -k #{key}"
+    def call(mod, key, opts={})
+      args = []
+      if s = opts[:search_type]
+        args << '-s' << s
+      end
+      if r = opts[:range]
+        args << '-r' << r
+      end
+      cmd = "diatheke -b #{mod} #{args.map {|a| Shellwords.escape(a.to_s)}.join(' ')} -k #{key}"
+      puts cmd
       `#{cmd}`
     end
 
